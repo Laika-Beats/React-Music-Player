a4a29f0a0194473611063b6a94c51cc4d610480d
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-24 14:22:35 -0700

    songs and currentSongs state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React, { useState } from "react";
 import "./styles/app.scss";
 import Player from "./components/Player";
 import Song from "./components/Song";
-// import data from "./util";
+import data from "./util";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  console.log(songs);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
